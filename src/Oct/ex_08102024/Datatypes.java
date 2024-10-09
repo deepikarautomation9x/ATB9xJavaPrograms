263b7b04de8334e4b4266b63fcae315a13fa5ebf
Deepika <drathore052020@gmail.com>
2024-10-09 21:58:56 +0530

    Push all 08

---
+++ src/Oct/ex_08102024/Datatypes.java
@@ -0,0 +1,23 @@
+package Oct.ex_08102024;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        // Primitive Data Type
+        byte b  = 100; // 8 bits, 1 Byte
+        short s  = 128; // 16 bits, 2 Byte
+        int i = 1000; // 32 bits, 4 Byte
+        long phone_number = 9876543210l; // 64 Bits, 8 Bytes
+        char grade = 'A'; // 'A', b, c , e, d, el -> ASCII value - type of int - assigned.
+        // 16 Bits, 2 Byte
+        float f2 = 3.14F; // 32 Bits, 4 Byte
+        double d = 118.9876543; // 64 Bites, 8 Bytes
+
+        // Store the distance between earth and moon
+        long distance_between_earth_moon = 384400000l; //in M
+        double distance_between_earth_moon2 = 384400000.865; //in M
+
+        // Non primitive Data Types (user defined)
+        String name = "Pramod"; // String - bunch of char(collection of chars)
+        // Array, Class- User Data Types. - 64 Bits, 8 Byte
+    }
+}
