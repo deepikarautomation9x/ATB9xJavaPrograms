555ce6bda6036fc63a5d2550757e579d53be43fa
Deepika <drathore052020@gmail.com>
2024-10-09 22:03:12 +0530

    Push task09

---
+++ src/Oct/ex_08102024/Task09.java
@@ -0,0 +1,33 @@
+package Oct.ex_08102024;
+
+public class Task09 {
+    public static void main(String[] args)
+    {
+        int a = 5;
+        System.out.printf("%d * %d = %d",a,1, a*1);
+        System.out.println();
+        System.out.printf("%d * %d = %d",a,1, a*2);
+        System.out.println();
+        System.out.printf("%d * %d = %d",a,1, a*3);
+        System.out.println();
+        System.out.printf("%d * %d = %d",a,1, a*4);
+        System.out.println();
+        System.out.printf("%d * %d = %d",a,1, a*5);
+        System.out.println();
+        System.out.printf("%d * %d = %d",a,1, a*6);
+        System.out.println();
+        System.out.printf("%d * %d = %d",a,1, a*7);
+        System.out.println();
+        System.out.printf("%d * %d = %d",a,1, a*8);
+        System.out.println();
+        System.out.printf("%d * %d = %d",a,1, a*9);
+        System.out.println();
+        System.out.printf("%d * %d = %d",a,1, a*10);
+        System.out.println();
+        System.out.printf("%d * %d = %d",a,1, a*11);
+        System.out.println();
+        System.out.printf("%d * %d = %d",a,1, a*12);
+    }
+}
+
+
