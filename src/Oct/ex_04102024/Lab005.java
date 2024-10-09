cd3db6603c0e267a263b5d746c8e3bfd6cfa50c2
Deepika <drathore052020@gmail.com>
2024-10-09 20:37:11 +0530

    Push all 04

---
+++ src/Oct/ex_04102024/Lab005.java
@@ -0,0 +1,8 @@
+package Oct.ex_04102024;
+
+public class Lab005 {
+    //Any argument in java can be passed
+    public static void main(String[] deepika) {
+
+    }
+}
---
+++ src/Oct/ex_04102024/Lab006.java
@@ -0,0 +1,9 @@
+package Oct.ex_04102024;
+
+public class Lab006 {
+    public static void main(String[] args)
+    {
+        System.out.println("Lab006");
+    }
+}
+
