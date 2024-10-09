829e71b6349cacdff5ea2dd0b52db8f40fee7a70
Deepika <drathore052020@gmail.com>
2024-10-09 22:06:19 +0530

    Push task09

--- src/Oct/ex_08102024/Task09.java
+++ src/Oct/ex_08102024/Task09.java
@@ -3,30 +3,26 @@ package Oct.ex_08102024;
 public class Task09 {
     public static void main(String[] args)
     {
-        int a = 5;
+        int a = 6;
         System.out.printf("%d * %d = %d",a,1, a*1);
         System.out.println();
-        System.out.printf("%d * %d = %d",a,1, a*2);
+        System.out.printf("%d * %d = %d",a,2, a*2);
         System.out.println();
-        System.out.printf("%d * %d = %d",a,1, a*3);
+        System.out.printf("%d * %d = %d",a,3, a*3);
         System.out.println();
-        System.out.printf("%d * %d = %d",a,1, a*4);
+        System.out.printf("%d * %d = %d",a,4, a*4);
         System.out.println();
-        System.out.printf("%d * %d = %d",a,1, a*5);
+        System.out.printf("%d * %d = %d",a,5, a*5);
         System.out.println();
-        System.out.printf("%d * %d = %d",a,1, a*6);
+        System.out.printf("%d * %d = %d",a,6, a*6);
         System.out.println();
-        System.out.printf("%d * %d = %d",a,1, a*7);
+        System.out.printf("%d * %d = %d",a,7, a*7);
         System.out.println();
-        System.out.printf("%d * %d = %d",a,1, a*8);
+        System.out.printf("%d * %d = %d",a,8, a*8);
         System.out.println();
-        System.out.printf("%d * %d = %d",a,1, a*9);
+        System.out.printf("%d * %d = %d",a,9, a*9);
         System.out.println();
-        System.out.printf("%d * %d = %d",a,1, a*10);
-        System.out.println();
-        System.out.printf("%d * %d = %d",a,1, a*11);
-        System.out.println();
-        System.out.printf("%d * %d = %d",a,1, a*12);
+        System.out.printf("%d * %d = %d",a,10, a*10);
     }
 }
 
