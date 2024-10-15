c47e05edc6a40c80a2d8c4bc7fb56fcdf8c86e0d
Deepika <drathore052020@gmail.com>
2024-10-15 20:26:34 +0530

    Increment/Decrement task 15 oct

---
+++ src/Oct/ex_14102024/Inctdecoperator.java
@@ -0,0 +1,14 @@
+package Oct.ex_14102024;
+
+public class Inctdecoperator {
+    public static void main(String[] args)
+    {
+        /* Task 1 -
+        int a = 10;
+        System.out.println(++a + a++ + a++); //(11,11),(11,12),(12,13)
+        System.out.println(a);*/
+       //Task 2-
+        int a = 20;
+        System.out.println(--a + a++ + a--); //(19,19)(19,20),(20 19) //output = 58
+    }
+}
