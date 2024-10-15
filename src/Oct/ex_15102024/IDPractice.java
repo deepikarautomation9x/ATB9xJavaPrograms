7310cfc8282be98065b9f07347b6bba4987a3b9a
Deepika <drathore052020@gmail.com>
2024-10-15 21:13:44 +0530

    Id practice 15 oct

---
+++ src/Oct/ex_15102024/IDPractice.java
@@ -0,0 +1,11 @@
+package Oct.ex_15102024;
+
+public class IDPractice {
+    public static void main(String[] args)
+    {
+        int m = 0, n = 0;
+        int p = --m * --n * n-- * m-- ;   // (-1,-1). (-1,-1) ,(-1.-2).(-1.-2)
+        System.out.println(p);
+
+    }
+}
