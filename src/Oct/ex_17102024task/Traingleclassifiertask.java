e85cfe1d391f003304245a91b23f96524d3c2ec9
Deepika <drathore052020@gmail.com>
2024-10-21 07:30:08 +0530

    17 th Oct task hackerrank

---
+++ src/Oct/ex_17102024task/Traingleclassifiertask.java
@@ -0,0 +1,29 @@
+package Oct.ex_17102024task;
+
+import java.util.Scanner;
+
+public class Traingleclassifiertask {
+    public static void main(String[] args) {
+       /* Triangle Classifier:
+        Write a program that classifies a triangle based on its side lengths.
+        Given three input values representing the lengths of the sides,
+        determine if the triangle is equilateral (all sides are equal),
+         isosceles (exactly two sides are equal), or scalene (no sides are equal).
+         Use an if-else statement to classify the triangle.
+                side1, side2, side2 → eq, iso, scalene */
+        //int side1 = 10, side2 = 20, side3 = 30;
+        //take input from three sides
+        Scanner sc = new Scanner(System.in);
+        int side1 = sc.nextInt();
+        int side2 = sc.nextInt();
+        int side3 = sc.nextInt();
+        if (side1 == side2 && side1 == side3) {
+            System.out.println("******Equilateral triangle ********");
+        }else if(side1 == side2 || side1 == side3 || side2 == side3) {
+            System.out.println("The triangle is isosceles.");
+        }else {
+            System.out.println("The triangle is scalene.");
+        }
+
+    }
+}
