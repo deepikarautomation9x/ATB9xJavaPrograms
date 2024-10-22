7dfe276c235a2855fdb96304504f39e964240f58
Deepika <drathore052020@gmail.com>
2024-10-22 07:35:07 +0530

    18th oct task2

---
+++ src/Tasks/Task2monthsusingswitch.java
@@ -0,0 +1,28 @@
+package Tasks;
+import java.util.Scanner;
+public class Task2monthsusingswitch {
+    public static void main(String[] args) {
+        //Write a program that takes an integer input (1-12) and prints the name of the corresponding
+        // month using a switch statement.
+        //Input  - 12
+        //Out - Dec.
+        System.out.println("Enter input from 1 to 12");
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        switch (month) {
+            case 1 -> System.out.println("Jan");
+            case 2 -> System.out.println("Feb");
+            case 3 -> System.out.println("Mar");
+            case 4 -> System.out.println("Apr");
+            case 5 -> System.out.println("May");
+            case 6 -> System.out.println("Jun");
+            case 7 -> System.out.println("July");
+            case 8 -> System.out.println("Aug");
+            case 9 -> System.out.println("Sep");
+            case 10 -> System.out.println("Oct");
+            case 11 -> System.out.println("Nov");
+            case 12 -> System.out.println("Dec");
+            default -> System.out.println(" Invalid input entered");
+        }
+    }
+}
