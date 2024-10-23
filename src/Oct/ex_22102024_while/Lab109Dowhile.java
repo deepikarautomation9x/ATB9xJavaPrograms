84f0eab98cef0c0a5603b60807e2e7eee77731a4
Deepika <drathore052020@gmail.com>
2024-10-23 06:01:14 +0530

    22nd oct class

---
+++ src/Oct/ex_22102024_while/Lab109Dowhile.java
@@ -0,0 +1,12 @@
+package Oct.ex_22102024_while;
+
+public class Lab109Dowhile {
+    public static void main(String[] args) {
+        int a = 0;
+        do {
+            System.out.println("Run a Code -> " + a);
+            a++;
+        } while (a < 10);
+    }
+}
+
---
+++ src/Oct/ex_22102024_while/Lab110d.java
@@ -0,0 +1,19 @@
+package Oct.ex_22102024_while;
+
+public class Lab110d {
+    public static void main(String[] args) {
+//        int a = 0;
+//        while (a < 0)
+//        {
+//            System.out.println("Print" + a);
+//            a++;
+//        }
+
+        int a = 0;
+        do {
+            System.out.println("Print" + a);
+            a++;
+        } while (a < 0);
+    }
+}
+
